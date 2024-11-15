732431e9fc903dc5f67e57b26bd1f53555c2d6d6
Pouria <p.dehghani@student.saedubai.com>
2024-11-16 01:52:35 +0400

    Modified card behaviors to fix some issues and prevent some more, should be more readable as well

--- Assets/_Scripts/Card.cs
+++ Assets/_Scripts/Card.cs
@@ -1,12 +1,9 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
     public bool facingUp = true;
-    public bool cardSelected = false;
-    
 
 
     public enum cardType
@@ -17,7 +14,8 @@ public class Card : MonoBehaviour
         christianHamster,
         dogLol,
         ebilCat
-    } 
+    }
+
     // Select the type of card it is in the inspector with this drop list.
     [SerializeField] private cardType _cardType;
 
@@ -25,29 +23,27 @@ public class Card : MonoBehaviour
     {
         return _cardType;
     }
-    
+
+    // Flip the current card.
     public void FlipCard()
     {
         if (facingUp)
-        {
-            StartCoroutine(FlipAnimation(new Vector3(0,0,180)));
-        }
+            StartCoroutine(FlipAnimation(new Vector3(0, 0, 180)));
         else
-        {
-            StartCoroutine(FlipAnimation(new Vector3(0,0,0)));
-        }
+            StartCoroutine(FlipAnimation(new Vector3(0, 0, 0)));
 
         facingUp = !facingUp;
     }
 
     // This is for the animation of the flip.
     [SerializeField] private float flipDuration = 0.5f;
-    // FUTURE - Make the animation slower
+
+    // Flip animation for the cards.
     private IEnumerator FlipAnimation(Vector3 targetRotation)
     {
-        float timeElapsed = 0f;
-        Quaternion startRotation = transform.rotation;
-        Quaternion endRotation = Quaternion.Euler(targetRotation);
+        var timeElapsed = 0f;
+        var startRotation = transform.rotation;
+        var endRotation = Quaternion.Euler(targetRotation);
 
         while (timeElapsed < flipDuration)
         {
@@ -55,7 +51,8 @@ public class Card : MonoBehaviour
             timeElapsed += Time.deltaTime;
             yield return null;
         }
-
+        
+        // Makes sure it gets to the correct end rotation (since its time dependant and not frame-rate dependant).
         transform.rotation = endRotation;
     }
 }
--- Assets/_Scripts/CardManager.cs
+++ Assets/_Scripts/CardManager.cs
@@ -1,25 +1,42 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class CardManager : MonoBehaviour
 {
-    // The time the cards are going to be show to the player at the beginning.
-    [SerializeField] private float hintTime;
-    [SerializeField] private float shuffleInterval = 5f;
-    [SerializeField] private float movingDuration = 1.5f;
-    
-    [SerializeField] private Transform[] cardPositions;
-    [SerializeField] private GameObject[] cards;
+    [Header("Game settings.")]
 
     // Pool that will contain all cards (will contain double the amount of the cards array).
     private readonly List<GameObject> cardPool = new();
-    
+
+    /// <summary>
+    ///     The time the cards are going to be show to the player at the beginning.
+    /// </summary>
+    [SerializeField] private float hintTime;
+
+    /// <summary>
+    ///     The interval between shuffles.
+    /// </summary>
+    [SerializeField] private float shuffleInterval = 5f;
+
+    /// <summary>
+    ///     The duration that will take two cards to change location.
+    /// </summary>
+    [SerializeField] private float movingDuration = 1.5f;
+
+    /// <summary>
+    ///     The transforms of the location in which the cards are going to be instantiated. Needs to be a pair number.
+    /// </summary>
+    [SerializeField] private Transform[] cardPositions;
+
+    /// <summary>
+    ///     Prefabs of the cards game objects.
+    /// </summary>
+    [SerializeField] private GameObject[] cards;
 
     #region Game Preparation
+
     private void Start()
     {
         // Just to check if the length of the transforms is not pair (which makes the game literally unplayable),
@@ -35,33 +52,33 @@ public class CardManager : MonoBehaviour
         StartCoroutine(ShuffleCardsPeriodically());
     }
 
-    private List<GameObject> instantiatedCards = new List<GameObject>();
+    private readonly List<GameObject> instantiatedCards = new();
+
     private void InstantiateCardsInShuffledPosition()
     {
         for (var i = 0; i < cardPositions.Length; i++)
         {
-            GameObject cardInstance = Instantiate(cardPool[i], cardPositions[i].position, Quaternion.Euler(0,0,0));
-            
+            var cardInstance = Instantiate(cardPool[i], cardPositions[i].position, Quaternion.Euler(0, 0, 0));
+
             // Set the instanced card as a child of the position object, to be able to access the card just by using the position.
             cardInstance.transform.SetParent(cardPositions[i]);
-            
+
             // DEBUG - Delete later.
             instantiatedCards.Add(cardInstance);
-            
-            Card cardScript = cardInstance.GetComponentInParent<Card>();
+
+            var cardScript = cardInstance.GetComponentInParent<Card>();
             if (cardScript != null)
-            {
-                StartCoroutine(FlipCardAfterDelay(cardScript, hintTime));
-            }
+                // Flips the card after showing a hint to the player.
+                StartCoroutine(EndShowingHint(cardScript, hintTime));
         }
     }
-    
-    private IEnumerator FlipCardAfterDelay(Card card, float delay)
+
+    private IEnumerator EndShowingHint(Card card, float delay)
     {
         yield return new WaitForSeconds(delay);
         card.FlipCard();
     }
-    
+
     private void ShuffleCards()
     {
         for (var i = 0; i < cardPool.Count; i++)
@@ -85,18 +102,27 @@ public class CardManager : MonoBehaviour
             cardPool.Add(card);
         }
     }
+
     #endregion
 
     #region Game Manager
 
-    public List<Card> selectedCards = new List<Card>();
+    public List<Card> selectedCards = new();
+
     // If the counter is the same as the amount of cards in the GameObject array of cards (line 10), then the game is won. (because all pairs are facing up)
     private int correctGuessesCounter;
     public bool isResetting;
-    public bool isShuffling = false;
+
     private void Update()
     {
-        if (selectedCards.Count >= 2 && !isShuffling)
+        SelectedCardsPairChecker();
+        CheckIfWon();
+    }
+
+    // Checks for two selected cards.
+    private void SelectedCardsPairChecker()
+    {
+        if (selectedCards.Count >= 2)
         {
             if (selectedCards[0].GetCardType() == selectedCards[1].GetCardType())
             {
@@ -107,16 +133,12 @@ public class CardManager : MonoBehaviour
             else
             {
                 Debug.Log("Not a match.");
-                if (!isResetting)
-                {
-                    StartCoroutine(ResetCard());
-                }
+                if (!isResetting) StartCoroutine(ResetCard());
             }
         }
-
-        CheckIfWon();
     }
 
+    // Reset the cards after a small period of time to make tell the player they didn't get a match.
     private IEnumerator ResetCard()
     {
         isResetting = true;
@@ -126,64 +148,61 @@ public class CardManager : MonoBehaviour
             yield return new WaitForSeconds(0.2f);
             card.FlipCard();
         }
+
         selectedCards.Clear();
         isResetting = false;
     }
 
-    
+
     // Checks if the game is won.
     private void CheckIfWon()
     {
-        if (correctGuessesCounter == cards.Length)
-        {
-            Debug.Log("Game won.");
-        }
+        if (correctGuessesCounter == cards.Length) Debug.Log("Game won.");
     }
 
     private IEnumerator ShuffleCardsPeriodically()
     {
-        // This keeps happening (add to a timer in the future).
+        // The true condition can be changed to a timer if it gets implemented.
         while (true)
         {
+            // If two cards are being reset, wait until they're done resetting.
             yield return new WaitUntil(() => !isResetting);
-            
+
+            // Start the shuffling after the shuffling interval.
             yield return new WaitForSeconds(Random.Range(shuffleInterval, shuffleInterval + 4));
 
             // The two positions that are going to be changing places.
-            int index1 = Random.Range(0, instantiatedCards.Count);
-            int index2 = Random.Range(0, instantiatedCards.Count);
+            var index1 = Random.Range(0, instantiatedCards.Count);
+            var index2 = Random.Range(0, instantiatedCards.Count);
 
             // This is to make sure that both positions are not the same.
-            while (index1 == index2)
-            {
-                index2 = Random.Range(0, instantiatedCards.Count);
-            }
+            while (index1 == index2) index2 = Random.Range(0, instantiatedCards.Count);
 
-            GameObject card1 = instantiatedCards[index1]; // Gets the first child which is the card occupying this space.
-            GameObject card2 = instantiatedCards[index2];
+            var card1 = instantiatedCards[index1]; // Gets the first child which is the card occupying this space.
+            var card2 = instantiatedCards[index2];
 
             // Correct the indexes of the cards in the list so it doesn't de-synchronize in the future.
             // (This was causing the bug of the cards being overlapped)
             instantiatedCards[index1] = card2;
             instantiatedCards[index2] = card1;
-            
-            StartCoroutine(AnimatePeriodicShuffle(card1.transform, cardPositions[index2].position, movingDuration));
-            StartCoroutine(AnimatePeriodicShuffle(card2.transform, cardPositions[index1].position, movingDuration));
+
+            // Animate the shuffle of the cards.
+            StartCoroutine(AnimateCardShuffle(card1.transform, cardPositions[index2].position, movingDuration));
+            StartCoroutine(AnimateCardShuffle(card2.transform, cardPositions[index1].position, movingDuration));
 
             yield return new WaitForSeconds(movingDuration);
-            
-            
         }
     }
 
-    private IEnumerator AnimatePeriodicShuffle(Transform card, Vector3 targetPosition, float animDuration)
+
+    private IEnumerator AnimateCardShuffle(Transform card, Vector3 targetPosition, float animDuration)
     {
-        Vector3 startPosition = card.position;
-        float timeElapsed = 0f;
+        var startPosition = card.position;
+        var timeElapsed = 0f;
 
         // To prevent sudden shifts (quick workaround for overlapping cards when the player interacts with cards)
         card.position = startPosition;
-        
+
         while (timeElapsed < animDuration)
         {
             card.position = Vector3.Lerp(startPosition, targetPosition, timeElapsed / animDuration);
@@ -191,8 +210,9 @@ public class CardManager : MonoBehaviour
             yield return null;
         }
 
+        // To prevent bugs.
         card.position = targetPosition;
     }
-    
+
     #endregion
 }
