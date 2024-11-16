91084d5f9b40bd726c7386ee3af68466059f0941
Pouria <p.dehghani@student.saedubai.com>
2024-11-16 09:17:53 +0400

    updated player interaction

--- Assets/_Scripts/PlayerInteraction.cs
+++ Assets/_Scripts/PlayerInteraction.cs
@@ -1,12 +1,10 @@
-using System;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
     private CardManager cardManager;
-    
+
     [SerializeField] private float rayCastDistance;
     [SerializeField] private GameObject crosshair;
     private Animator crosshairAnimator;
@@ -37,29 +35,40 @@ public class PlayerInteraction : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, rayCastDistance))
         {
-            if (hit.transform.CompareTag($"Card"))
+            if (hit.transform.CompareTag("Card"))
             {
-                crosshairAnimator.SetBool("Interacting",true);
-                crosshairImage.color = Color.green;
-                
-                // If the player clicks and the cards are not being reset, can flip the card the player is looking at.
-                if (Input.GetKeyUp(KeyCode.Mouse0) && !cardManager.isResetting)
-                {
-                    Card cardInteracted = hit.transform.GetComponentInParent<Card>();
-                    
-                    // This checks if the card the player is interacting with is not the same they already clicked.
-                    if (!cardManager.selectedCards.Contains(cardInteracted))
-                    {
-                        cardInteracted.FlipCard();
-                        cardManager.selectedCards.Add(cardInteracted);
-                    }
-
-                }
+                CrosshairInteractionAnimation(true);
+                HandleCardInteraction(hit);
             }
-            else if (!hit.transform.CompareTag($"Card") || hit.transform == null)
+            else if (!hit.transform.CompareTag("Card") || hit.transform == null)
             {
-                crosshairAnimator.SetBool("Interacting",false);
-                crosshairImage.color = Color.white;
+                CrosshairInteractionAnimation(false);
+            }
+        }
+    }
+
+    // Updates the crosshair to tell the player whatever they're looking at is interactable.
+    private void CrosshairInteractionAnimation(bool isInteractable)
+    {
+        crosshairAnimator.SetBool("Interacting", isInteractable);
+        if (isInteractable)
+            crosshairImage.color = Color.green;
+        else
+            crosshairImage.color = Color.white;
+    }
+
+    // If the player clicks and the cards are not being reset, can flip the card the player is looking at.
+    private void HandleCardInteraction(RaycastHit hit)
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0) && !cardManager.isResetting)
+        {
+            var cardInteracted = hit.transform.GetComponentInParent<Card>();
+
+            // This checks if the card the player is interacting with is not the same they already clicked.
+            if (!cardManager.selectedCards.Contains(cardInteracted))
+            {
+                cardInteracted.FlipCard();
+                cardManager.selectedCards.Add(cardInteracted);
             }
         }
     }
